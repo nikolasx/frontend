1dc437c77519b97b8755a9261f8050883c0695af
nikolasx <lixiong0901@163.com>
2017-03-23 19:42:08 +0800

    11111

---
+++ webpack-study/dist/js/msg.js
@@ -0,0 +1,17 @@
+webpackJsonp([1],{
+
+/***/ 16:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/**
+ * Created by lix10 on 2017/3/23.
+ */
+
+__webpack_require__(0);
+
+/***/ })
+
+},[16]);
